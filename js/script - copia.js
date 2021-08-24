0c69ace6abeb03c374cc5a0ffc8f2f14ab3f47fc
Yesenia <alejandraredfg3@gmail.com>
2021-08-24 08:27:25 -0500

    musica

--- js/script - copia.js
+++
@@ -1,7 +0,0 @@
-
-const audio = document.getElementById("audio")
-const play_btn = document.getElementById("play")
-play_btn.addEventListener('click',()=>{
-    audio.play();
-    image.style.animationPlayState = "running"
-})
